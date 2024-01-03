679c87ab2fdad01f746422795c746019d0b617ce
Chandan8585 <chandanwingshr@gmail.com>
2024-01-03 22:10:00 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
-import { ParallaxBanner, useParallax } from "react-scroll-parallax";
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParallax } from "react-scroll-parallax";
 import Island from "../src/images/island.jpg";
 import Beach from "../src/images/Beach.jpg";
 import Ocean2 from "../src/images/ocean2.jpg";
 import Ocean from "../src/images/ocean.jpg";
 import wave from "../src/images/wave.jpg";
 import ship from "../src/images/ship.jpg";
-import fish from "../src/images/firsh.jpg";
-import dolphin from "../src/images/dolphin.jpg";
+import dolphin from "../src/images/dolphin.png";
+
 const App = () => {
   const [showNavbar, setShowNavbar] = useState(false);
 
@@ -17,50 +17,41 @@ const App = () => {
       const scrollY = window.scrollY || document.documentElement.scrollTop;
       setShowNavbar(scrollY > window.innerHeight);
     };
+
     window.addEventListener('scroll', handleScroll);
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
-
-  
   const parallaxRotateY = useParallax({
     rotateY: [0, 360],
   });
-  
+
   const waveParallax = useParallax({
-    translateX: [-50, 0], // Adjust these values as needed
+    translateX: [-50, 0],
   });
-  
+
   const shipParallax = useParallax({
-    translateX: [50, 0], // Adjust these values as needed
+    translateX: [50, 0],
   });
-  const parallaxEasing = useParallax({
-    easing: "easeOutQuad",
-    translateX: [-340, 100],
+
+  const parallaxDolphin2 = useParallax({
+    translateY: [0, -window.innerHeight * 0.5],
+    translateX: [0, 300, { easing: 'linear' }],
+    rotateZ: [0, 360 * 3, { easing: 'linear' }],
   });
-  const parallaxEasingLeft = useParallax({
-    easing: [1, -0.75, 0.5, 1.34],
-    translateX: [0, -260],
-    translateY: [1100],
-  });
-  const  parallaxFish = useParallax({
-    easing: "ç",
-    translateX: [0, 170], // Adjust these values as needed
-  });
-  
+
   const parallaxDolphin = useParallax({
-    easing: "easeOutQuad",
-    translateX: [1700, 0],// Adjust these values as needed
+    translateY: [0, -window.innerHeight * 1.5],
+    translateX: [50, 300, { easing: 'linear' }],
+    rotateZ: [0, 360 * 3, { easing: 'linear' }],
   });
-  
+
   return (
     <div>
       <header className={showNavbar ? 'show-navbar' : ''}>
-        <div className="logo">
-          <img src="https://img.freepik.com/free-vector/branding-identity-corporate-vector-logo-m-design_460848-10168.jpg" />
-        </div>
         <nav>
           <li>Home</li>
           <li>About</li>
@@ -75,8 +66,8 @@ const App = () => {
           src={Island}
         />
         <div className="absolute-text">
-          <h1 ref={parallaxEasing.ref}>HEY THERE</h1>
-          <h2 ref={parallaxEasingLeft.ref}>LET'S DEEP DIVE INTO THE NATURE</h2>
+          <h1>HEY THERE</h1>
+          <h2>LET'S DEEP DIVE INTO THE NATURE</h2>
         </div>
       </section>
       <section className="bg-container">
@@ -84,7 +75,6 @@ const App = () => {
           ref={parallaxRotateY.ref}
           src={Beach}
         />
-  
       </section>
       <section className="card-container">
         <div className="card" ref={waveParallax.ref}>
@@ -94,32 +84,25 @@ const App = () => {
           <img src={ship} alt="Ship" />
         </div>
       </section>
-
       <section className="bg-container">
         <img
-          // ref={parallaxRotateY.ref}
           src={Ocean2}
           className="ocean2"
         />
-  
       </section>
       <section className="bg-container">
         <div className="container-body">
-          <img src={fish} alt="Fish" className="fish" ref={parallaxFish.ref} />
           <img src={dolphin} alt="Dolphin" className="dolphin" ref={parallaxDolphin.ref} />
+          <img src={dolphin} alt="Dolphin" className="dolphin" ref={parallaxDolphin2.ref} />
         </div>
       </section>
-     
-    
       <section className="bg-container">
         <img
-         
           src={Ocean}
         />
-  
       </section>
-      
     </div>
   );
 };
+
 export default App;
